26a6a7b2c5f46101a19d898f18d3e11c4901b884
DricomDragon <contact@jovian-hersemeule.eu>
2019-10-15 15:20:02 +0200

    Split file TPmongofiche in two different files
    In order to have one single purpose for each file.

--- prim_list.py
+++ prim_list.py
@@ -9,16 +9,11 @@ import pymongo as pm
 import pprint
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.merigold
 collection = db.customer    
-print("Affiche de la fiche de Jacques WEBER")
-print("\n")
-for fiche in collection.find({'forname': 'Jacques','name': 'WEBER'}):
-    pprint.pprint(fiche)
-print("\n")
+
 print("Affiche des fiches de la société Primeur & co")
-print("\n")
-    
 for fiche in collection.find({'society': 'Primeur & co'}):
     pprint.pprint(fiche)
