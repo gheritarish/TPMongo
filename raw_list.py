eea4014b11ce10fe05333cc5801dfc30fecbe644
DricomDragon <contact@jovian-hersemeule.eu>
2019-10-15 15:12:11 +0200

    Improve code format

--- raw_list.py
+++ raw_list.py
@@ -1,9 +1,10 @@
 import pymongo as pm
 import pprint
-
 from pymongo import MongoClient
-client = MongoClient('localhost',27017)
+
+client = MongoClient('localhost', 27017)
 db = client.merigold
 collection = db.customer
+
 for fiche in collection.find():
     pprint.pprint(fiche)
